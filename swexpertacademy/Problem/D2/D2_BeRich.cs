0e53d30965ab3ca0e677833b42bcd4836370bf3f
suhunkim <tngns3927@naver.com>
2024-01-02 15:19:36 +0900

    no message

---
+++ swexpertacademy/Problem/D2/D2_BeRich.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class D2_BeRich
+	{
+		static void Main(string[] args)
+		{
+
+			string sCount = Console.ReadLine();
+			int iCount = Convert.ToInt32(sCount);
+
+			for (int i = 0; i < iCount; i++)
+			{
+				string sCase = Console.ReadLine();
+				int iCase = Convert.ToInt32(sCase);
+
+				string sPrice = Console.ReadLine();
+				int[] iPrice = sPrice.Split(' ');
+			}
+
+
+		}
+
+		private void inputN()
+		{
+
+		}
+	}
+}
